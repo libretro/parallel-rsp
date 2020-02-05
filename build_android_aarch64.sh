8acb0fbcacd6d12a6c6de58d906275a15a9ac8c4
Hans-Kristian Arntzen <maister@archlinux.us>
2020-02-05 17:10:46 +0100

    Add integration so that we can run ctest on Android and compare.

--- build_android_aarch64.sh
+++ build_android_aarch64.sh
@@ -7,6 +7,10 @@ fi
 
 mkdir -p build-android-aarch64
 cd build-android-aarch64
-cmake .. -DCMAKE_TOOLCHAIN_FILE="$ANDROID_HOME/ndk-bundle/build/cmake/android.toolchain.cmake" -DANDROID_ABI=arm64-v8a -DCMAKE_BUILD_TYPE=Debug -DANDROID_PLATFORM=android-26
+cmake .. -DCMAKE_TOOLCHAIN_FILE="$ANDROID_HOME/ndk-bundle/build/cmake/android.toolchain.cmake" \
+	-DANDROID_ABI=arm64-v8a \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DANDROID_PLATFORM=android-26 \
+	-DPARALLEL_RSP_DEBUG_JIT=OFF
 cmake --build . --parallel
 cd ..
