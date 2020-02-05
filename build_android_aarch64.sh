36cd023e59981b5d7e1fdeab71ae9592f8beb9fa
Hans-Kristian Arntzen <maister@archlinux.us>
2020-02-05 16:31:37 +0100

    Android aarch64 builds.

---
+++ build_android_aarch64.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z $ANDROID_HOME ]; then
+	echo "Please define ANDROID_HOME environment variable to where Android SDK is installed, usually ~/Android/Sdk."
+	exit 1
+fi
+
+mkdir -p build-android-aarch64
+cd build-android-aarch64
+cmake .. -DCMAKE_TOOLCHAIN_FILE="$ANDROID_HOME/ndk-bundle/build/cmake/android.toolchain.cmake" -DANDROID_ABI=arm64-v8a -DCMAKE_BUILD_TYPE=Debug -DANDROID_PLATFORM=android-26
+cmake --build . --parallel
+cd ..
