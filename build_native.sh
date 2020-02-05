6bb5782e1a3b5201fbbd6ad45557cf15046cdf9e
Hans-Kristian Arntzen <maister@archlinux.us>
2020-02-05 19:34:38 +0100

    Add a COP2 VU fuzzer which runs randomized opcodes.

--- build_native.sh
+++ build_native.sh
@@ -2,6 +2,6 @@
 
 mkdir -p build-native
 cd build-native
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DPARALLEL_RSP_DEBUG_JIT=OFF
+cmake .. -DCMAKE_BUILD_TYPE=Release -DPARALLEL_RSP_DEBUG_JIT=OFF
 cmake --build . --parallel
 cd ..
